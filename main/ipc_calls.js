32163306faec03e678ee7f6f0bae0701d3789f61
mikerashoo <mkbirhanu@gmail.com>
2022-12-19 11:35:04 +0300

    git changes made

--- main/ipc_calls.js
+++
@@ -1,4 +0,0 @@
-export const USER_CRUD_CALLS = {
-    getAll: 'get-all-users',
-    create: 'create-user',
-}
--- renderer/services/osapi.js
+++
@@ -1,4 +0,0 @@
-import electron from 'electron'; 
-const osApi = electron.ipcRenderer || false;
-
-export default osApi;
